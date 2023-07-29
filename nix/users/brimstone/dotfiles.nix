6aebbfa7389c874898178a53c9528064f3dc4063
justinlime <justinlime1999@gmail.com>
2023-07-28 21:25:15 -0500

    edits

--- nix/users/brimstone/dotfiles.nix
+++ nix/users/brimstone/dotfiles.nix
@@ -125,7 +125,7 @@ in
         framerate = 60;
         autosens = 1;
         sensitivity = 100;
-        bars = 12;
+        bars = 18;
         lower_cutoff_freq = 50;
         higher_cutoff_freq = 10000;
         method = "pipewire";
