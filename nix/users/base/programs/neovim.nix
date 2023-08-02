77e2f8ccf7004f02cce0c5ce4441faa821c0e31e
justinlime <justinlime1999@gmail.com>
2023-08-01 19:52:53 -0500

    test

--- nix/users/base/programs/neovim.nix
+++ nix/users/base/programs/neovim.nix
@@ -1,6 +1,26 @@
-{ self , ... }:
+{ pkgs, ... }:
 {
+    home.packages = with pkgs; [
+      neovim
+      gcc
+
+      #language servers:
+      lua-language-server #Lua
+      nil #Nix
+      gopls #Golang
+      rust-analyzer #Rust
+      zls #Zig
+      llvmPackages_15.clang-unwrapped #C, C++
+      python311Packages.jedi-language-server #Python
+      nodePackages_latest.vscode-langservers-extracted #HTML,CSS, JSON
+      nodePackages_latest.grammarly-languageserver #Markdown
+      nodePackages_latest.typescript-language-server #Javascript and Typescript
+      nodePackages_latest.bash-language-server #Bash
+      nodePackages_latest.dockerfile-language-server-nodejs #Dockerfiles
+      nodePackages_latest.yaml-language-server #Yaml
+    ];
+
     xdg.configFile = {
-        "nvim".source = "${self}/.config/nvim";
+        "nvim".source = ../../../../.config/nvim;
     };
 }
