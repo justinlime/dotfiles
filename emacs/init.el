9e33b909295d2a2a5aa42252f883c58f1facb984
justinlime <justinlime1999@gmail.com>
2023-10-01 17:58:27 -0500

    emacs fix

--- emacs/init.el
+++ emacs/init.el
@@ -1,7 +1,10 @@
-;; (org-babel-tangle-file
-;; (expand-file-name
-;;   "config.org"
-;  user-emacs-directory))
+(setq gc-cons-threshold 100000000)
+;; Needed because org-babel-load-file refuses to
+;; regenerate config.el when config.org is a symlink
+(delete-file
+(expand-file-name
+  "config.el"
+ user-emacs-directory))
 
 (org-babel-load-file
  (expand-file-name
