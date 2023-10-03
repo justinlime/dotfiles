ef88834836de70a20a3e9c7ff003c74068668589
justinlime <justinlime1999@gmail.com>
2023-10-02 20:18:18 -0500

    emacs ssh

--- nix/users/base/zsh.nix
+++ nix/users/base/zsh.nix
@@ -16,6 +16,16 @@
       }
       setopt PROMPT_SUBST
       PROMPT='%B%F{183}%m%f%F{111}[%f%F{158}%~%f%F{111}]%f%F{111}$(parse_git_branch)%f %F{183}>%f%f%b '
+			if [[ "$TERM" == "dumb" ]]
+			then
+				unsetopt zle
+				unsetopt prompt_cr
+				unsetopt prompt_subst
+				unfunction precmd
+				unfunction preexec
+				PS1='$ '
+			fi
+
     '';
   };
 }
