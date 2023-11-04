15b579df816ba4f233775fea1758709b88413a2b
justinlime <justinlime1999@gmail.com>
2023-11-04 15:59:15 -0500

    edits

--- prepare-repo.sh
+++ prepare-repo.sh
@@ -6,11 +6,9 @@
 # them visible to git and nix. This is done incase the files might have sensitive information,
 # or if they have imperative settings like my flake (setting the user and system profile).
 
-git add --intent-to-add flake.nix
 git add --intent-to-add nix/systems/stinkserver/containers/wireguard.nix
 git add --intent-to-add nix/systems/stinkserver/containers/duckdns.nix
 
-git update-index --assume-unchanged flake.nix
 git update-index --assume-unchanged nix/systems/stinkserver/containers/wireguard.nix
 git update-index --assume-unchanged nix/systems/stinkserver/containers/duckdns.nix
 
