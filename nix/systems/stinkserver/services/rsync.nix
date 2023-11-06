0a3494601f8d06abf7d720f2b732efab4dd68706
justinlime <justinlime1999@gmail.com>
2023-11-05 18:53:44 -0600

    rsync edit

--- nix/systems/stinkserver/services/rsync.nix
+++ nix/systems/stinkserver/services/rsync.nix
@@ -1,39 +1,35 @@
 { pkgs, ... }:
 {
-  systemd.timers."rsync_users" = {
-    wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnBootSec = "5m";
-        OnUnitActiveSec = "5m";
-        Unit = "rsync_users.service";
+  systemd = {
+    timers = {
+      "rsync_users" = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          Persistent = true; 
+          OnCalendar = "hourly";
+          Unit = "rsync_users.service";
+        };
       };
-  };
-  systemd.timers."rsync_users_full" = {
-    wantedBy = [ "timers.target" ];
-      timerConfig = {
-        OnBootSec = "5m";
-        OnUnitActiveSec = "5m";
-        Unit = "rsync_users_full.service";
+      "rsync_users_full" = {
+        wantedBy = [ "timers.target" ];
+        timerConfig = {
+          Persistent = true; 
+          OnCalendar = "weekly";
+          Unit = "rsync_users_full.service";
+        };
       };
-  };
-  systemd.services."rsync_users" = {
-    script = ''
-    ${pkgs.rsync}/bin/rsync -rv --ignore-existing /drives/NVME0/users /storage/pool
-    '';
-    serviceConfig = {
-      OnCalendar = "*-*-* *:00:00";
-      User = "root";
-      Persistent = true; 
     };
-  };
-  systemd.services."rsync_users_full" = {
-    script = ''
-    ${pkgs.rsync}/bin/rsync -rv /drives/NVME0/users /storage/pool
-    '';
-    serviceConfig = {
-      OnCalendar = "weekly";
-      User = "root";
-      Persistent = true; 
+    services = {
+      "rsync_users" = {
+        script = ''
+          ${pkgs.rsync}/bin/rsync -rv --ignore-existing /drives/NVME0/users /storage/pool
+        '';
+      };
+      "rsync_users_full" = { 
+        script = ''
+          ${pkgs.rsync}/bin/rsync -rv /drives/NVME0/users /storage/pool
+        '';
+      };
     };
   };
 }
