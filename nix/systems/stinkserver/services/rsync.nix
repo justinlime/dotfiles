1670b4d22982e54afc7d1d982dc8c672d3ded102
justinlime <justinlime1999@gmail.com>
2023-11-06 07:10:12 -0600

    edits

--- nix/systems/stinkserver/services/rsync.nix
+++ nix/systems/stinkserver/services/rsync.nix
@@ -22,7 +22,7 @@
     services = {
       "rsync_users" = {
         script = ''
-          ${pkgs.rsync}/bin/rsync -rv --ignore-existing /drives/NVME0/users /storage/pool
+          ${pkgs.rsync}/bin/rsync -rv --ignore-existing /drives/NVME0/users /storage/pool 
         '';
       };
       "rsync_users_full" = { 
