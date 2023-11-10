cd50743533d61915b35287cf89a7f45a61789b8a
justinlime <justinlime1999@gmail.com>
2023-11-10 13:37:58 -0600

    flake edit

--- flake.nix
+++ flake.nix
@@ -23,23 +23,34 @@
   outputs = { self, nixpkgs, nixpkgs_stable, home-manager, ... }@inputs:
   let
     username = "justinlime";
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    pkgs_stable = nixpkgs_stable.legacyPackages.${system};
 
     # The path to this very repo 
     flake_path = "/home/${username}/dotfiles";
 
+    allSystems = x: [
+      (x + ".x86_64-linux")
+      (x + ".x86_64-darwin")
+      (x + ".aarch64-linux")
+    ];
+
     allHomeConfigurations =  
-      nixpkgs.lib.genAttrs [
+      nixpkgs.lib.genAttrs (nixpkgs.lib.flatten (nixpkgs.lib.lists.forEach [
         "brimstone"
         "janus"
-      ] (profile: 
+      ] allSystems)) # This will generate an entry for each profile and system, Example: brimstone.x86_64-linux
+      (profile: 
+          let
+            split = nixpkgs.lib.strings.splitString "." profile;
+            name = builtins.elemAt split 0;
+            system =  builtins.elemAt split 1;
+            pkgs = nixpkgs.legacyPackages.${system};
+            pkgs_stable = nixpkgs_stable.legacyPackages.${system};
+          in
           home-manager.lib.homeManagerConfiguration {
             inherit pkgs;
             extraSpecialArgs = { inherit profile username flake_path pkgs_stable inputs; };
             modules = [
-              ./nix/users/${profile}
+              ./nix/users/${name}
               # Pin registry to flake
               { nix.registry.nixpkgs.flake = nixpkgs; }
               # Pin channel to flake 
@@ -47,16 +58,24 @@
             ];
           });
     allSystemConfigurations = 
-      nixpkgs.lib.genAttrs [
+      nixpkgs.lib.genAttrs (nixpkgs.lib.flatten (nixpkgs.lib.lists.forEach [
         "stinkserver"
         "jesktop"
         "japtop"
-      ] (profile: 
+      ] allSystems))
+      (profile: 
+           let
+             split = nixpkgs.lib.strings.splitString "." profile;
+             name = builtins.elemAt split 0;
+             system =  builtins.elemAt split 1;
+             pkgs = nixpkgs.legacyPackages.${system};
+             pkgs_stable = nixpkgs_stable.legacyPackages.${system};
+           in
            nixpkgs.lib.nixosSystem {
-             # inherit system;
+             inherit system;
              specialArgs = { inherit profile username flake_path pkgs_stable inputs; }; 
              modules = [
-               ./nix/systems/${profile}
+               ./nix/systems/${name}
                { nix.registry.nixpkgs.flake = nixpkgs; }
                { nix.nixPath = [ "nixpkgs=configflake:nixpkgs" ]; }
              ];
