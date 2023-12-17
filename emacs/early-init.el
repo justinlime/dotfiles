ac2fba351b241c9bf1cda31807cc23d3f2f3c49d
justinlime <justinlime1999@gmail.com>
2023-12-16 22:51:13 -0600

    garbage collector edit

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -1,1 +1,2 @@
+(setq gc-cons-threshold (* 1024 1024 200)) ;200 MB GC
 (setq package-enable-at-startup nil)
--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,3 @@
-(setq gc-cons-threshold (* 1024 1024 200)) ;200 MB GC
-
 ;; Needed because org-babel-load-file refuses to
 ;; regenerate config.el when config.org is a symlink
 (delete-file
