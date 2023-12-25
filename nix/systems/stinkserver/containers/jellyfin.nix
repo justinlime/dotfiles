6828aa664f806b66e8c4cc4e268a0ef0128268a8
justinlime <justinlime1999@gmail.com>
2023-12-25 16:39:47 -0600

    jellyfin edit

--- nix/systems/stinkserver/containers/jellyfin.nix
+++ nix/systems/stinkserver/containers/jellyfin.nix
@@ -19,6 +19,7 @@
        "/storage/pool/media/watch/movies:/movies"
        "/storage/pool/media/watch/tv:/tv"
        "/storage/pool/media/watch/anime:/anime"
+       "/storage/pool/media/watch/mom:/mom"
     ];
      extraOptions = [ "--network=host" "--device=/dev/dri/renderD128:/dev/dri/renderD128" "--device=/dev/dri/card0:/dev/dri/card0" ];
     };  
