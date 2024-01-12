1d043eafc733ae9002fb2f8fabca1e4979af6ea5
justinlime <justinlime1999@gmail.com>
2024-01-12 07:45:01 -0600

    requestrr

--- nix/systems/stinkserver/containers/requestrr.nix
+++ nix/systems/stinkserver/containers/requestrr.nix
@@ -3,7 +3,7 @@
   virtualisation.oci-containers.containers = {
     requestrr = {
      autoStart = true; 
-     image = "darkalfx/requestrr";
+     image = "darkalfx/requestrr:latest";
      ports = [ "4545:4545" ];
      volumes = [
        "/configs/requestrr:/root/config"
