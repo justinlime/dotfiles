faa2a1c0d5ed8fa8ae25b50b438f192479953c09
justinlime <justinlime1999@gmail.com>
2024-02-24 19:15:58 -0600

    ports for serve

--- nix/systems/base/networking.nix
+++ nix/systems/base/networking.nix
@@ -3,6 +3,8 @@
   # Enable the firewall
   networking = {
     firewall = {
+      allowedTCPPorts = [ 3000 ];
+      allowedUDPPorts = [ 3000 ];
       enable = true;
       checkReversePath = false; 
     };
