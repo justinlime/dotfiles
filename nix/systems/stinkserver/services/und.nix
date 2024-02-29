c5b13d064a38690101da235e47574e24034146de
justinlime <justinlime1999@gmail.com>
2024-02-28 20:41:42 -0600

    server edits

--- nix/systems/stinkserver/services/und.nix
+++ nix/systems/stinkserver/services/und.nix
@@ -5,12 +5,12 @@
     serviceConfig = {
       User=username;
       Group=username;
-      WorkingDirectory="/home/${username}";
-      EnvironmentFile="/home/${username}/.und_mainchain/cosmovisor/UND_COSMOVISOR_ENV";
+      WorkingDirectory="/drives/EXTERNAL1";
+      EnvironmentFile="/drives/EXTERNAL1/und_archive/cosmovisor/UND_COSMOVISOR_ENV";
       Restart="always";
       RestartSec="10s";
       LimitNOFILE=4096;
-      ExecStart = "/usr/local/bin/cosmovisor run start";
+      ExecStart = "/usr/local/bin/cosmovisor run start --home /drives/EXTERNAL1/und_archive";
     };
     wantedBy = [ "default.target" ];
   };
@@ -65,6 +65,51 @@
         }];
         locations."/" = {
           proxyPass = "http://localhost:9169";
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;    
+          '';
+        };
+      };
+      "rpc.unification.refundvalidator.com" = {
+        serverName = "rpc.unification.refundvalidator.com";
+        listen = [{
+          port = 90;
+          addr = "0.0.0.0";
+        }];
+        locations."/" = {
+          proxyPass = "http://localhost:26657";
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;    
+          '';
+        };
+      };
+      "rest.unification.refundvalidator.com" = {
+        serverName = "rest.unification.refundvalidator.com";
+        listen = [{
+          port = 90;
+          addr = "0.0.0.0";
+        }];
+        locations."/" = {
+          proxyPass = "http://localhost:1317";
+          extraConfig = ''
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;    
+          '';
+        };
+      };
+      "grpc.unification.refundvalidator.com" = {
+        serverName = "grpc.unification.refundvalidator.com";
+        listen = [{
+          port = 90;
+          addr = "0.0.0.0";
+        }];
+        locations."/" = {
+          proxyPass = "http://localhost:9090";
           extraConfig = ''
             proxy_set_header Host $host;
             proxy_set_header X-Real-IP $remote_addr;
