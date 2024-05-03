50cd7c79f84b95c5015dcf57b6939e7a70ec3267
justinlime <justinlime1999@gmail.com>
2024-05-02 22:57:48 -0500

    optimize

--- emacs/early-init.el
+++ emacs/early-init.el
@@ -3,7 +3,10 @@
 ;; IDK what this does tbh, but seems to help startup times a bit
 (setq file-name-handler-alist-original file-name-handler-alist)
 (setq file-name-handler-alist nil)
+(setq frame-inhibit-implied-resize t)
 ;; disable package.el at early init
 ;; even though it will be loaded later
 ;; shaves off some startup time
-(setq package-enable-at-startup nil)
+(setq package-enable-at-startup nil ; don't auto-initialize!
+      ;; don't add that `custom-set-variables' block to my init.el!
+      package--init-file-ensured t)
