ac43fdbbc87d03926196a3e6d50396653d7694cc
justinlime <justinlime1999@gmail.com>
2024-05-20 03:25:29 -0500

    smart edit

--- nix/systems/base/smart.nix
+++ nix/systems/base/smart.nix
@@ -3,7 +3,7 @@
   services.smartd = {
     enable = true;
     autodetect = true;
-    # Run short self tests daily, long self tests weekly
-    defaults.autodetected = "-a -o on -s (S/../.././02|L/../../7/04)";
+    # Run short self tests daily, long self tests monthly
+    defaults.autodetected = "-a -o on -s (S/../.././02|L/01/../../04)";
   };
 }
