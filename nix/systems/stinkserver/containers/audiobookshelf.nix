b13a8eec8ced6b39ce857da86d9437a6550900f4
justinlime <justinlime1999@gmail.com>
2025-05-30 15:57:10 -0500

    again

--- nix/systems/stinkserver/containers/audiobookshelf.nix
+++ nix/systems/stinkserver/containers/audiobookshelf.nix
@@ -45,21 +45,6 @@
             proxy_buffering off;
           '';
         };
-        locations."/socket" = {
-          proxyPass = "http://localhost:1337";
-          extraConfig = ''
-            # Proxy Jellyfin Websockets traffic
-            proxy_http_version 1.1;
-            proxy_set_header Upgrade $http_upgrade;
-            proxy_set_header Connection "upgrade";
-            proxy_set_header Host $host;
-            proxy_set_header X-Real-IP $remote_addr;
-            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-            proxy_set_header X-Forwarded-Proto $scheme;
-            proxy_set_header X-Forwarded-Protocol $scheme;
-            proxy_set_header X-Forwarded-Host $http_host;
-          '';
-        };
       };
     };
   };
