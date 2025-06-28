2edb747d0347c73a4e8cf3d27d9caa92127616ca
justinlime <justinlime1999@gmail.com>
2025-06-28 09:23:59 -0500

    extensions and typos

--- nix/modules/systems/gnomerdp.nix
+++ nix/modules/systems/gnomerdp.nix
@@ -7,6 +7,9 @@ let cfg = config.sysMods.gnomerdp; in
   config = lib.mkIf cfg.enable {
     networking.firewall.allowedTCPPorts = [ 3389 ];
     environment.systemPackages = with pkgs; [
+      gnomeExtensions.dash-to-dock
+      gnomeExtensions.arcmenu
+      gnomeExtensions.tray-icons-reloaded
       gnome-remote-desktop
       freerdp
       firefox
@@ -22,19 +25,14 @@ let cfg = config.sysMods.gnomerdp; in
     };
     services.gnome.gnome-remote-desktop.enable = true;
     # These commands must be run imperatively below for the first setup.
-    # Some may error out, dont worry.
-    # This example will let you login as "name" "password"
 
-    # sudo rm -rf ~gnome-remote-desktop/rdp-tls*
     # sudo -u gnome-remote-desktop winpr-makecert -silent -rdp -path ~gnome-remote-desktop rdp-tls
 
     # sudo grdctl --system rdp set-tls-key /var/lib/gnome-remote-desktop/rdp-tls.key
     # sudo grdctl --system rdp set-tls-cert /var/lib/gnome-remote-desktop/rdp-tls.crt
 
-    # sudo systemctl daemon-reload
-    # sudo grdctl --system rdp set-credentials "name" "password"
+    # sudo grdctl --system rdp set-credentials
 
-    # sudo systemctl restart gnome-remote-desktop.service
-    # 
+    # sudo systemctl restart gnome-remote-desktop
   };
 }
